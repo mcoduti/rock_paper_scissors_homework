c17a784e2dbc1d852f057e50a88008d4228c8667
mcoduti <matthew.coduti@gmail.com>
2015-07-26 16:23:54 -0500

    no database features yet

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,4 +6,32 @@ class GameController < ApplicationController
 
   # Your code goes below.
 
+
+  def rps
+    @user_move = params["the_move"]
+
+    @computer_move =["rock","paper","scissors"].sample
+
+      if @user_move == @computer_move
+      @outcome = "Tie!"
+    elsif @user_move == "paper" && @computer_move == "rock"
+      @outcome = "You won!"
+    elsif @user_move == "paper" && @computer_move == "scissors"
+      @outcome = "You lost!"
+    elsif @user_move == "scissors" && @computer_move == "rock"
+      @outcome = "You lost!"
+    elsif @user_move == "scissors" && @computer_move == "paper"
+      @outcome = "You won!"
+    elsif @user_move == "rock" && @computer_move == "paper"
+      @outcome = "You lost!"
+    elsif @user_move == "rock" && @computer_move == "scissors"
+      @outcome = "You won!"
+    end
+
+
+
+
+    render("move.html.erb")
+  end
+
 end
